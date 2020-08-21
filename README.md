94ef6edf638b488d31b0039f2adfbd8b87d57139
shuwenkai <774932198@163.com>
2020-08-21 16:47:44 +0800

    配置修改，更改默认入口

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 [![Build Status](https://travis-ci.org/shuwenkai/ts-group-sort.svg?branch=master)](https://travis-ci.org/shuwenkai/ts-group-sort)
 # 数组分组排序
 
-> 传入数组，升序或者降序，排序分组字段，将分组排序后输出
+> 传入数组，升序或者降序，排序分组字段，将分组排序后输出 travis-ci 持续集成
 
+>实例
 ```bash
     const testData=[{
         id:1,
@@ -25,37 +26,6 @@
         label:'label4',
         group:2
     }]
-    const resultData=[
-        {
-            group:1,
-            sort:1,
-            children:[{
-                id:1,
-                sort:1,
-                label:'label1',
-                group:1
-            },{
-                id:2,
-                sort:2,
-                label:'label2',
-                group:1
-            }],
-        },
-        {
-            group:2,
-            sort:3,
-            children:[{
-                id:3,
-                sort:3,
-                label:'label3',
-                group:2
-            },{
-                id:4,
-                sort:4,
-                label:'label4',
-                group:2
-            }],
-        },
-    ]
     const result = sort(testData, 1,'group','sort');
+
 ```
--- test/test.js
+++ test/test.js
@@ -3,61 +3,61 @@ const expect = require('chai').expect;
 const sort = require('../dist/index').sort;
 
 describe('ts-group-sort function test', () => {
-  it('should return array', () => {
-      const testData=[{
-        id:1,
-        sort:1,
-        label:'label1',
-        group:1
-    },{
-        id:2,
-        sort:2,
-        label:'label2',
-        group:1
-    },{
-        id:3,
-        sort:3,
-        label:'label3',
-        group:2
-    },{
-        id:4,
-        sort:4,
-        label:'label4',
-        group:2
-    }]
-    const resultData=[
-        {
-            group:1,
-            sort:1,
-            children:[{
-                id:1,
-                sort:1,
-                label:'label1',
-                group:1
-            },{
-                id:2,
-                sort:2,
-                label:'label2',
-                group:1
-            }],
-        },
-        {
-            group:2,
-            sort:3,
-            children:[{
-                id:3,
-                sort:3,
-                label:'label3',
-                group:2
-            },{
-                id:4,
-                sort:4,
-                label:'label4',
-                group:2
-            }],
-        },
-    ]
-    const result = sort(testData, 1,'group','sort');
-    expect(result).to.not.equal(resultData);
-  });
+    it('should return array', () => {
+        const testData = [{
+            id: 1,
+            sort: 1,
+            label: 'label1',
+            group: 1
+        }, {
+            id: 2,
+            sort: 2,
+            label: 'label2',
+            group: 1
+        }, {
+            id: 3,
+            sort: 3,
+            label: 'label3',
+            group: 2
+        }, {
+            id: 4,
+            sort: 4,
+            label: 'label4',
+            group: 2
+        }]
+        const resultData = [
+            {
+                group: 1,
+                sort: 1,
+                children: [{
+                    id: 1,
+                    sort: 1,
+                    label: 'label1',
+                    group: 1
+                }, {
+                    id: 2,
+                    sort: 2,
+                    label: 'label2',
+                    group: 1
+                }],
+            },
+            {
+                group: 2,
+                sort: 3,
+                children: [{
+                    id: 3,
+                    sort: 3,
+                    label: 'label3',
+                    group: 2
+                }, {
+                    id: 4,
+                    sort: 4,
+                    label: 'label4',
+                    group: 2
+                }],
+            },
+        ]
+        const result = sort(testData, 1, 'group', 'sort');
+        expect(result).to.not.equal(resultData);
+    });
 });
